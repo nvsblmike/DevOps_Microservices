52b93db6df878a3bb5b13b86a828da56d1e33083
nvsblmike <soundboylesh77@gmail.com>
2022-09-02 21:35:07 +0000

    final stages, project 4

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=nvsblmike/nvsbl
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run nvsbl\
+    --image=$dockerpath\
+    --port=80 --labels app=nvsbl
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward nvsbl 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=nvsblmike/nvsbl
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login
+docker --tag nvsbl $dockerpath:latest
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
